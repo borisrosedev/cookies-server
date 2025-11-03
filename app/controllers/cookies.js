31c84f7d502a578d46eeac09e3babd1c1a745b11
borisrosedev <boris.rose.dev@gmail.com>
2025-11-03 23:17:04 +0100

    feat/log

--- app/controllers/cookies.js
+++ app/controllers/cookies.js
@@ -2,14 +2,19 @@
 const cookiesController = {
 
     get(req, res){
+        console.log("-*-------------------")
         console.log(req.query)
-
+        console.log("-*-------------------")
         return res.json({ payload : req.query?? req.params })
     },
 
 
     post(req, res){
         const { cookies } = req.body 
+
+        console.log("--------------------")
+        console.log(req.cookies)
+        console.log("--------------------")
         return res.json({ cookies })
     }
 
